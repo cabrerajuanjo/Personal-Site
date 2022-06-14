b77f7923838aec08f9cf890cb71960f3f49a3df4
Juan José Cabrera <cabrera57juanjo@gmail.com>
2022-06-14 00:52:40 -0300

    Checking of input file type and image preview

--- public/js/main.js
+++ public/js/main.js
@@ -1,16 +1,100 @@
-const showInputImage = () => {
-    const imageInput = document.getElementById("image_file")
-    var uploadedImage = ""
+const bufferArrayArrayComparator =  (bufferBinaryArray, array) => {
+    bufferSignature = bufferBinaryArray.slice(0, array.length);
+    console.log(bufferSignature)
+    if (bufferSignature.length != array.length)
+    {
+        return false
+    }
+    for (var i = 0; i < array.length; i++) {
+        if (bufferSignature[i] != array[i]){
+            return false;
+        }
+    }
+    return true;
+}
+
+const enableUpload = () => {
+    console.log("enableUpload")
+    document.getElementById("upload_image_button").removeAttribute("disabled");
+}
+
+const disableUpload = () => {
+    document.getElementById("upload_image_button").setAttribute('disabled', '');
+}
+
+const showInputImage = (imageInput) => {
     const reader = new FileReader()
+    reader.readAsDataURL(imageInput.files[0])
     reader.addEventListener("load", () => {
         uploadedImage = reader.result
-        //document.getElementById("input_img").style.backgroundImage = 'url(' + uploadedImage + ')'
         htmlContentToAppend = '<img src="' + uploadedImage + '" alt="">'
         document.getElementById("input_img").innerHTML = htmlContentToAppend
     })
-    reader.readAsDataURL(imageInput.files[0])
+}
+
+const preprocessFile = () => {
+    //Show image on browser
+    const imageInput = document.getElementById("image_file")
+
+
+
+    //Checks for valid image type
+    const buffer = new FileReader()
+    buffer.readAsArrayBuffer(imageInput.files[0])
+
+    buffer.addEventListener("load", () => {
+        bufferBinaryArray = new Uint8Array(buffer.result)
+
+        pngFileSignature = [ 137, 80, 78, 71, 13, 10, 26 ];
+        jpgFileSignature = [255, 216, 255];
+        ppmFileSignature = [80, 54];
+        pgmFileSignature = [80, 50];
+
+        if(bufferArrayArrayComparator(bufferBinaryArray, pngFileSignature))
+        {
+            console.log("Image is PNG");
+            if(document.getElementById("show_preview").checked){
+                showInputImage(imageInput);
+            }
+            enableUpload();
+
+        }else if(bufferArrayArrayComparator(bufferBinaryArray, jpgFileSignature))
+        {
+            console.log("Image is JPG");
+            if(document.getElementById("show_preview").checked){
+                showInputImage(imageInput);
+            }
+            enableUpload();
+        }else if(bufferArrayArrayComparator(bufferBinaryArray, ppmFileSignature))
+        {
+            console.log("Image is PPM");
+            //showInputImage(imageInput);
+            enableUpload();
+        }else if(bufferArrayArrayComparator(bufferBinaryArray, pgmFileSignature))
+        {
+            console.log("Image is PGM");
+            //showInputImage(imageInput);
+            enableUpload();
+        }else{
+            disableUpload();
+        }
+        console.log("end");
+    })
+
+
 }
 
 document.addEventListener("DOMContentLoaded", (e) => {
-
+    disableUpload();
+    document.getElementById("image_file").value = "";
+    showPreview = document.getElementById("show_preview")
+    showPreview.addEventListener("change", (e) => {
+        if(!showPreview.checked)
+        {
+            document.getElementById("input_img").innerHTML = "";
+        }else
+        {
+            preprocessFile();
+        }
+    })
 })
